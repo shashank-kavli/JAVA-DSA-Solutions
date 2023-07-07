6786f283967545b662f4fd307e28173d640bf1da
shashank-kavli <121170732+shashank-kavli@users.noreply.github.com>
2023-07-07 16:36:57 +0530

    Update LC34.java

--- Arrays/LC34.java
+++ Arrays/LC34.java
@@ -1,4 +1,5 @@
 //34. Find First and Last Position of Element in Sorted Array
+//https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
 
 import java.util.Arrays.*;
 
