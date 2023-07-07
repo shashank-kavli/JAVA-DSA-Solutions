4f4dcd59412f40c2e349f5f74b578b0b403576a6
shashank-kavli <121170732+shashank-kavli@users.noreply.github.com>
2023-07-07 15:35:24 +0530

    Create LC744.java

---
+++ Arrays/LC744.java
@@ -0,0 +1,25 @@
+public class LC744{
+	public static void main(String[] args) {
+		char[] letters = {'c','f','j'};
+		char target = 'a';
+		int ans = ceiling(letters,target);
+		System.out.println(letters[ans]);
+	}
+	static int ceiling(char[] letters, char target){
+		int start = 0;
+		int end = letters.length - 1;
+		while(start <= end) {
+			int mid = start + (end - start)/2;
+			if (letters[mid] > target){
+				end = mid - 1;
+			}else {
+				start = mid + 1;
+			}
+		}
+		if (start == letters.length){
+			return 0;
+		}else{
+			return start;
+		}	
+	}
+}
